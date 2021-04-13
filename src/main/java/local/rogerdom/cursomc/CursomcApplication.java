055c431dc8ce43a5cbe9b5568534f5b5e515d0c2
roger-mcz <roger.corpore@gmail.com>
2021-04-13 11:27:13 -0300

    criada operaçcao de instanciação, gravado no H2, bugs na issue

--- src/main/java/local/rogerdom/cursomc/CursomcApplication.java
+++ src/main/java/local/rogerdom/cursomc/CursomcApplication.java
@@ -1,13 +1,37 @@
 package local.rogerdom.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import local.rogerdom.cursomc.domain.Categoria;
+import local.rogerdom.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "INFORMÁTICA");
+		Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+		
+		
+		
 }
