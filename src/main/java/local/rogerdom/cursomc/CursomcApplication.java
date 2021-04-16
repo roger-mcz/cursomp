8856f1095e1b66fe5baf45d7c2c655b63b8b60aa
roger-mcz <roger.corpore@gmail.com>
2021-04-16 08:44:28 -0300

    Produto associacao muitos pra muitos no MySQL

--- src/main/java/local/rogerdom/cursomc/CursomcApplication.java
+++ src/main/java/local/rogerdom/cursomc/CursomcApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import local.rogerdom.cursomc.domain.Categoria;
+import local.rogerdom.cursomc.domain.Produto;
 import local.rogerdom.cursomc.repositories.CategoriaRepository;
+import local.rogerdom.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,11 +30,22 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(null, "outros");
-	//	Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
+		Categoria cat1 = new Categoria(null, "INFORMATICA");
+		Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 		
---
+++ src/main/java/local/rogerdom/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package local.rogerdom.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import local.rogerdom.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
