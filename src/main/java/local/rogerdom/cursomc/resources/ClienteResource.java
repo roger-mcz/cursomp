4e9d2e7404db582f1435050aa8755ae7d8b1c436
roger-mcz <roger.corpore@gmail.com>
2021-04-19 20:52:12 -0300

    Endpoint /clientes/{id} disponível proteção json serialized ok

---
+++ src/main/java/local/rogerdom/cursomc/resources/ClienteResource.java
@@ -0,0 +1,36 @@
+package local.rogerdom.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import local.rogerdom.cursomc.domain.Cliente;
+import local.rogerdom.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")	//(nome do endPoint Rest)
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente cliente = service.buscar(id);
+		
+		return ResponseEntity.ok().body(cliente);
+				
+		/* metodo antigo 
+		 * Categoria cat1 = new Categoria(1, "Informatica"); Categoria cat2 = new
+		 * Categoria(2, "Escritorio");
+		 * 
+		 * List <Categoria> lista = new ArrayList<>(); lista.add(cat1); lista.add(cat2);
+		 * return lista;
+		 */
+	}
+
+}
