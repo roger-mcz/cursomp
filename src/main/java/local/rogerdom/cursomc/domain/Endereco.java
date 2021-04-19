c69092c060ec064cfebb1a0577e7d5dcbbdcf10d
roger-mcz <roger.corpore@gmail.com>
2021-04-19 20:52:12 -0300

    cliente, tipoCliente(enum), tel., end.

--- src/main/java/local/rogerdom/cursomc/domain/Endereco.java
+++ src/main/java/local/rogerdom/cursomc/domain/Endereco.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco implements Serializable{
 
@@ -23,13 +25,19 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cidade_id")
 	private Cidade cidade;
+	
+	public Endereco() {
+		
+	}
 
 	public Endereco(Integer id, String logradouro, String num, String complemento, String bairro, String cep,
 			Cliente cliente, Cidade cidade) {
@@ -100,13 +108,15 @@ public class Endereco implements Serializable{
 		this.cliente = cliente;
 	}
 
-	public Cidade getCidade() {
-		return cidade;
+	
+	public Cidade getCidade() { 
+		return cidade; 
 	}
-
+	 
 	public void setCidade(Cidade cidade) {
-		this.cidade = cidade;
+		this.cidade = cidade; 
 	}
+	
 
 	@Override
 	public int hashCode() {
